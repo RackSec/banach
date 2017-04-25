aab18accd297eb55a0fb3ff30f3da7546bff0dfe
lvh <_@lvh.io>
2017-04-24 22:19:56 -0500

    Upgrade deps

--- project.clj
+++ project.clj
@@ -5,13 +5,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [manifold "0.1.4"]
+                 [manifold "0.1.6"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
-  :plugins [[lein-auto "0.1.2"]
-            [lein-cljfmt "0.3.0"]
-            [lein-pprint "1.1.1"]
-            [lein-environ "1.0.2"]
+  :plugins [[lein-auto "0.1.3"]
+            [lein-cljfmt "0.5.6"]
+            [lein-pprint "1.1.2"]
+            [lein-environ "1.1.0"]
             [lein-ancient "0.6.10"]
-            [lein-cloverage "1.0.7-SNAPSHOT"]]
+            [lein-cloverage "1.0.9"]]
   :cljfmt {:indents {let-flow [[:inner 0]]
                      catch [[:inner 0]]}})
